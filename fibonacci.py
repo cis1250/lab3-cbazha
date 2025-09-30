81605647eb5fd89410cf5f08e2c8264bbb40fdcc
cbazha <cbazha@uoguelph.ca>
2025-09-30 10:53:45 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -6,4 +6,9 @@
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
 
-Input("How many terms of the fibonacci sequence do you want?")
+input = Input("How many terms of the fibonacci sequence do you want?")
+if (input<=0):
+  Input("Please enter a positive integer.")
+
+for num in input:
+  
